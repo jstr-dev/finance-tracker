90b9e623ce713db8c761ec731d8503cc83cf1c80
justa <adamcgl@outlook.com>
2025-04-12 00:02:08 +0100

    Can fetch investments from trading 212 and store in database.

--- app/Services/Trading212Service.php
+++ app/Services/Trading212Service.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\UserConnection;
+use App\Models\UserInvestment;
 use Cache;
 use Http;
 
@@ -82,19 +83,46 @@ class Trading212Service {
 
     public function sync(UserConnection $conn)
     {
+        $syncedAt = now();
+
+        $instruments = Cache::remember('trading212.instruments', 60 * 60 * 24 * 30, function () use ($conn) {
+            $instruments = $this->getInstruments($conn);
+            $instruments = collect($instruments);
+            return $instruments->keyBy('ticker');
+        });
+
         $portfolio = $this->getPortfolio($conn);
-        dd($portfolio);
+        $investments = [];
 
-        dd();
-        $pies = Cache::remember("trading212.pies.{$conn->id}", 60, fn() => $this->getAllPies($conn));
-        $this->log('Fetching Pie information.');
-
-        foreach ($pies as $pie) {
-            $pieId = $pie['id'];
-            $this->log("Syncing Pie $pieId.");
-            $pie = $this->getPie($conn, $pieId);
-            dd($pie);
+        foreach ($portfolio as $investment) {
+            $this->log("Syncing Investment {$investment['ticker']}");
+            $instrument = $instruments->get($investment['ticker']);
+            $investments[] = [
+                'user_id' => $conn->user_id,
+                'connection_id' => $conn->id,
+                'ticker' => $investment['ticker'],
+                'name' => $instrument['name'] ?? null,
+                'amount' => $investment['quantity'],
+                'current_price' => $investment['currentPrice'],
+                'average_price' => $investment['averagePrice'],
+                'currency' => $instrument['currencyCode'],
+                'synced_at' => $syncedAt,
+            ];
         }
+
+        UserInvestment::upsert($investments, ['user_id', 'connection_id', 'ticker', 'synced_at'], [
+            'amount', 'average_price', 'current_price'
+        ]);
+
+        // $pies = Cache::remember("trading212.pies.{$conn->id}", 60, fn() => $this->getAllPies($conn));
+        // $this->log('Fetching Pie information.');
+
+        // foreach ($pies as $pie) {
+        //     $pieId = $pie['id'];
+        //     $this->log("Syncing Pie $pieId.");
+        //     $pie = $this->getPie($conn, $pieId);
+        //     dd($pie);
+        // }
     }
 
     private function log(string $msg)
--- database/migrations/2025_04_06_152427_add__investments_table.php
+++ database/migrations/2025_04_06_152427_add__investments_table.php
@@ -11,7 +11,30 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::dropIfExists('user_investments');
+        Schema::create('user_investments', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('connection_id');
+            $table->foreign('connection_id')->references('id')->on('user_connections')->onDelete('cascade');
+
+            $table->string('ticker');
+            $table->string('name')->nullable();
+            $table->decimal('amount', 32, 16);
+            $table->decimal('average_price', 32, 16);
+            $table->decimal('current_price', 32, 16);
+            $table->decimal('current_value', 32, 16)->storedAs('amount * current_price');
+            $table->string('currency')->default('GBP');
+            $table->date('synced_at');
+
+            $table->unique(['user_id', 'connection_id', 'ticker', 'synced_at']);
+
+            $table->index(['user_id', 'connection_id', 'synced_at', 'ticker'], 'search');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +42,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('user_investments');
     }
 };
