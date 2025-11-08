5cbdad6abfa0d1d6907cea133212bafd95463066
justa <adamcgl@outlook.com>
2025-11-08 23:33:30 +0000

    refactor connections page

--- resources/js/pages/connections/index.tsx
+++ resources/js/pages/connections/index.tsx
@@ -31,23 +31,20 @@ function ConnectionCard({ connection, isActive}: { connection: Connection, isAct
     }
 
     return (
-        <Card className={cn("h-full p-4 gap-4", 
+        <Card className={cn("px-4 py-3 gap-4 border-[1.5px] shadow-none", 
             isActive && 'border-green-200'
         )}>
-            <CardHeader className="p-0">
-                <div className='flex flex-row justify-between items-start'>
-                    <img src={connection.image} className="h-8 w-8 rounded-md mb-2" />
-                    {isActive && <Badge variant={"success"} className="h-6">Active</Badge>}
+            <CardHeader className="p-0 flex-row flex items-center justify-between">
+                <div className="flex-row flex gap-4 items-center">
+                    <img src={connection.image} className="h-8 w-8 rounded-md" />
+                    <div className="flex flex-col gap-0.5">
+                        <CardTitle className="text-sm">{connection.name}</CardTitle>
+                        <CardDescription className="text-xs">{connection.description}</CardDescription>
+                    </div>
                 </div>
-                <CardTitle className="text-sm">{connection.name}</CardTitle>
-                <CardDescription className="text-xs">{connection.description}</CardDescription>
+                <Button className="w-16 text-xs hover:cursor-pointer" variant="outline" size="sm"
+                    onClick={onClick} onMouseEnter={onHover}>{isActive ? 'Manage' : 'Connect'}</Button>
             </CardHeader>
-            <CardFooter className="p-0 w-full flex flex-row items-end h-full">
-                <div className="w-full flex flex-row">
-                    <Button className="w-16 text-xs hover:cursor-pointer" variant="outline" size="sm"
-                        onClick={onClick} onMouseEnter={onHover}>{isActive ? 'Manage' : 'Connect'}</Button>
-                </div>
-            </CardFooter>
         </Card>
     );  
 }
@@ -55,30 +52,8 @@ function ConnectionCard({ connection, isActive}: { connection: Connection, isAct
 export default function Connections({ connections, userConnections }: ConnectionsProps) {
     const [search, setSearch] = useState<string>('');
 
-    /**
-     * TODO: make this faster. (cache active)
-     */
     const isActiveConn = (connectionId: string) => {
         return userConnections.filter(uc => uc === connectionId).length > 0;
-    }
-
-    // TODO: remove
-    if (false) {
-        const generateRandomConnections = (count: number): Connection[] => {
-            const randomConnections: Connection[] = [];
-            for (let i = 0; i < count; i++) {
-                randomConnections.push({
-                    id: 'id_' + i,
-                    name: `Connection ${i + 1}`,
-                    description: `Description for connection ${i + 1}`,
-                    image: 'img.png',
-                });
-            }
-            return randomConnections;
-        };
-
-        // Add 10 random connection objects
-        connections = [...connections, ...generateRandomConnections(10)];
     }
 
     connections = connections.filter((connection: Connection) => connection.name.toLowerCase().includes(search.toLowerCase()));
@@ -87,7 +62,8 @@ export default function Connections({ connections, userConnections }: Connection
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Connections" />
-            <div className="max-w-5xl mx-auto flex w-full flex-col gap-6 p-4 mt-2">
+
+            <div className="max-w-3xl mx-auto flex w-full flex-col gap-6 p-4 mt-2">
                 <div className="w-full">
                     <Input
                         placeholder="Search connections..."
@@ -97,7 +73,7 @@ export default function Connections({ connections, userConnections }: Connection
                         onChange={(e) => setSearch(e.target.value)}
                     />
                 </div>
-                <div className="grid w-full gap-4 grid-cols-3 max-[1200px]:grid-cols-3 max-[1050px]:grid-cols-2 max-[600px]:grid-cols-1 auto-rows-max">
+                <div className="w-full gap-2 flex flex-col max-h-[80vh] overflow-y-auto">
                     {connections.map((connection: Connection) => (
                         <div key={connection.id}>
                             <ConnectionCard connection={connection} isActive={isActiveConn(connection.id)} />
